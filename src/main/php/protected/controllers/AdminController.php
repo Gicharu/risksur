9fa2b56d384a43531f94680547910fad8077341e
eric_thuku <eric@tracetracker.com>
2014-11-07 15:40:43 +0300

    RSKSR-20: Added page to list Options

--- src/main/php/protected/controllers/AdminController.php
+++ src/main/php/protected/controllers/AdminController.php
@@ -37,5 +37,67 @@ class AdminController extends Controller {
 	public function init() {
 		$this->configuration = Yii::app()->tsettings;
 	}
+
+	/**
+	 * actionIndex 
+	 * 
+	 * @access public
+	 * @return void
+	 */
+	public function actionIndex() {
+		Yii::log("actionIndex AdminController called", "trace", self::LOG_CAT);
+		$model = new Options;
+		$dataArray = array();
+		$dataArray['dtHeader'] = "Manage Options";
+		// Get list of options 
+		$optionsList = Options::model()->findAll(array(
+			'select' => 'optionId, label',
+		));
+		$optionsListArray = array();
+		// format datatable data
+		foreach ($optionsList as $options) {
+				$editButton = "<button id='editOption" . $options->optionId . 
+				"' type='button' class='bedit' onclick=\"window.location.href ='" . CController::createUrl('option/editOption/', array(
+					'designId' => $options->optionId)
+				) .
+				"'\">Edit</button>";
+				$deleteButton = "<button id='deleteOption" . $options->optionId . 
+				"' type='button' class='bdelete' onclick=\"$('#deleteBox').dialog('open');" . 
+				"deleteConfirm('" . $options->label . "', '" .
+				$options->optionId . "')\">Remove</button>";
+					//}
+			$optionsListArray[] = array (
+				'label' =>   $options->label,
+				'editButton' => $editButton,
+				'deleteButton' => $deleteButton
+			);
+		}
+		$dataArray['optionsList'] =  json_encode($optionsListArray);
+
+		// if (!empty($_GET['getDesigns'])) {
+		// 	$jsonData = json_encode(array("aaData" => $surveillanceListArray));
+		// 	echo $jsonData;
+		// 	return ;
+		// }
+		// // print_r($dataArray['surveillanceList']);die();
+		// // fetch the goal dropdown data
+		// $goalDropDown = GoalData::model()->findAll(array(
+		// 	'select' => 'pageId, pageName',
+		// 	'condition' => 'parentId=:parentId AND pageName<>:pageName',
+		// 	'params' => array(
+		// 		':parentId' => 0,
+		// 		':pageName' => 'noMenu'
+		// 	),
+		// ));
+		// // create array options for goal dropdown
+		// foreach ($goalDropDown as $data) {
+		// 	$dataArray['goalDropDown'][$data->pageId] = $data->pageName;
+		// }
+		$this->render('index', array(
+			'model' => $model,
+			'dataArray' => $dataArray
+		));
+	}
+
 }
 ?>
