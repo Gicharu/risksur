9fcaa69bfd86cf3a321703452de3cf25df8b6fe7
eric_thuku <eric@tracetracker.com>
2014-11-17 11:10:08 +0300

    RSKSR-20: Re-added SQL data that had been removed

---
+++ src/main/resources/MySQL/201411171109_RISKSUR_MySQLData.sql
@@ -0,0 +1,15 @@
+INSERT INTO `programpages` (`pageId`, `pageName`, `path`, `parentId`, `menuOrder`, `target`, `active`) VALUES
+(12,'Manage Select Options','options/index',8,15,'',1);
+
+
+INSERT INTO `permissions`(`id`,`name`,`description`,`controller`,`action`,`bizrule`) values 
+(23,'Manage Options','Manage Options','options','index',''),
+(24,'Add Options','Add Options','options','addOption',''),
+(25,'Edit Options','Edit Options','options','editOption',''),
+(26,'Delete Options','Delete Options','options','deleteOption','');
+
+INSERT INTO `roles_has_permissions`(`permissions_id`,`roles_id`) values
+(23,1),(23,2),
+(24,1),(24,2),
+(25,1),(25,2),
+(26,1),(26,2);
