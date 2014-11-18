45c8251fd008b93e2acd4b0af85608e13586b2b5
eric_thuku <eric@tracetracker.com>
2014-11-18 09:57:32 +0300

    RSKSR-20: Added styling to menus

--- src/main/php/protected/views/admin/goal.php
+++ src/main/php/protected/views/admin/goal.php
@@ -1,8 +1,12 @@
 <h3><?php echo $dataArray['formType']; ?> Goal</h3>
 <?php
-$this->menu = array(array('label' => 'Create Goal', 'url' => array('admin/addGoal')),);
-$this->menu = array(array('label' => 'View Goal', 'url' => array('admin/listGoals')),);
+$this->menu = array(array('label' => 'Create Goal', 'url' => array('admin/addGoal')), array('label' => 'View Goals', 'url' => array('admin/listGoals')));
 ?>
+<script type="text/javascript">
+	$(function() {
+		$("#bd").attr('style', '');
+	});
+</script>
 <div class="form">
 <?php 
 echo $form->render();
