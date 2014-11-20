c4e95f81d6364f62f1ba745e0e8d1642e23eb0ab
Chirag Doshi <chirag@tracetracker.com>
2014-11-20 11:42:58 +0300

    RSKSR-28 updated the ui and created duplicate action

--- src/main/php/protected/views/design/componentList.php
+++ src/main/php/protected/views/design/componentList.php
@@ -70,7 +70,8 @@ $(function(){
 			{"mDataProp": "description", "bVisible": true},
 			{"mDataProp": "description", "bVisible": true},
 			{"mDataProp": "editButton", "bVisible": true},
-			{"mData": "deleteButton", "bSortable": false, "bVisible": true },
+			{"mDataProp": "deleteButton", "bSortable": false, "bVisible": true },
+			{"mDataProp": "duplicateButton", "bSortable": false, "bVisible": true },
 		],
 		// update the buttons stying after the table data is loaded
 		"fnDrawCallback": function() {
@@ -78,6 +79,8 @@ $(function(){
 				icons: {primary: "ui-icon-trash"}, text: false});
 			$('button.bedit').button({
 				icons: {primary: "ui-icon-pencil"}, text: false});
+			$('button.bcopy').button({
+				icons: {primary: "ui-icon-copy"}, text: false});
 		},
 		"bJQueryUI": true,
 		//"sPaginationType": "customListbox",
@@ -98,7 +101,90 @@ $(function(){
 				var componentId = aData['componentId'];
 		window.location.href = '<?php echo CController::createUrl("design/showComponent"); ?>' + "?compId=" + componentId;
 	});
+
+	$("#copyBox").dialog({
+		autoOpen: false,
+		//bgiframe: true,
+		resizable: false,
+		overlay: {
+			backgroundColor: '#000',
+			opacity: 0.5
+		},
+		height: 300,
+		width: 350,
+		modal: true,
+		buttons: {
+			"Duplicate Component": duplicateComponent,
+			Cancel: function() {
+				$(this).dialog("close");
+				updateTips("");
+			}
+		},
+		close: function() {
+			$('#copyBoxForm')[ 0 ].reset();
+			updateTips("");
+			//allFields.removeClass( "ui-state-error" );
+		}
+	});
+
+	$("#copyBoxForm").on("submit", function(event) {
+		event.preventDefault();
+		duplicateComponent();
+	});
 });
+
+	function updateTips( t ) {
+		if (t != "") {
+			$(".validateTips")
+			.text( t )
+			.addClass( "ui-state-highlight" );
+			setTimeout(function() {
+				$(".validateTips").removeClass( "ui-state-highlight", 1500 );
+			}, 500 );
+			$('#newComponentName').addClass("ui-state-error");
+		} else {
+			$(".validateTips").text( t );
+			$('#newComponentName').removeClass("ui-state-error");
+		}
+	}
+	// process the duplicate function
+	duplicateComponent = function() {
+		if ( $('#newComponentName').val().length > 0) {
+			var oldCompId = $('#componentId').val();
+			var newCompName = $('#newComponentName').val();
+			updateTips("");
+			$("#copyBox").dialog("close");
+			var opt = {'loadMsg': 'Processing duplicate component'};
+			$("#listComponent").showLoading(opt);
+			$.ajax({type: 'POST',
+				url: <?php echo "'" . CController::createUrl('design/duplicateComponent') . "'"; ?>,
+				data: {oldComponentId:oldCompId,newComponentName:newCompName},
+				success: function(data){
+					var checkSuccess = /successfully/i;
+					if (checkSuccess.test(data)) {
+						// add process message
+						$("#ajaxFlashMsg").html(data);
+						$("#ajaxFlashMsgWrapper").attr('class', 'flash-success').show();
+					} else{
+						// add process message
+						$("#ajaxFlashMsg").html(data);
+						$("#ajaxFlashMsgWrapper").attr('class', 'flash-error').show();
+					}
+					clist.fnReloadAjax("listComponents/getComponents/1");
+					$("#listComponent").hideLoading();
+				},
+					error: function(data){
+						$("#ajaxFlashMsg").html("Error occured while deleting data");
+						$("#ajaxFlashMsgWrapper").attr('class', 'flash-error').show();
+						//console.log("error occured while posting data" + data);
+						$("#listComponent").hideLoading();
+					},
+						dataType: "text"
+			});
+		} else {
+			updateTips("The value of new component name cannot be empty.");
+		}
+	}
 
 	deleteConfirm = function(confirmMsg, deleteVal) {
 	$('#deleteBox').html("<p>Are you sure you want to delete '" + confirmMsg + "' </p>");
@@ -139,9 +225,13 @@ $(function(){
 			}
 	});
 }
+
+	function duplicatePopup(oldComponentId, oldComponentName){
+		$('#headerCopyBox').html("<p><?php echo Yii::t('translation', 'Please provide a new component name for copy of ')?> '" + oldComponentName + "' </p>");
+		$('#componentId').val(oldComponentId);
+	}
 	</script>
 <div id="listComponent" width="100%">
-	
 	<table id="componentList" width="100%" border="0" cellspacing="0" cellpadding="0">
 		<thead>
 		<tr>
@@ -150,9 +240,23 @@ $(function(){
 			<th title = "Details">Details</th>
 			<th title = "Edit">Edit</th>
 			<th title = "Delete">Delete</th>
+			<th title = "Duplicate">Duplicate</th>
 		</tr>
 		</thead>
 		<tbody>
 		</tbody>
 	</table>
 </div>
+
+<div id="copyBox" title="Duplicate Component">
+	<span id="headerCopyBox">Duplicate component</span>
+	<p class="validateTips" style="font-size:11px;color:red"></p>
+
+	<form id="copyBoxForm" width="100%">
+		<label for="name">New component Name</label>
+		<input type="text" name="newComponentName" id="newComponentName" value="" class="text ui-widget-content ui-corner-all" placeholder="New Name"/>
+		<input type="hidden" name="componentId" id="componentId" value="" class="text ui-widget-content ui-corner-all"/>
+		<!-- Allow form submission with keyboard without duplicating the dialog button -->
+		<!--<input type="submit" tabindex="-1" style="position:absolute; top:-1000px">-->
+	</form>
+</div> 
