bdf34754faf38a5ef87efa3b6ea8237ad083646c
Chirag Doshi <chirag@tracetracker.com>
2014-12-30 09:57:56 +0300

    RSKSR-30 updated the permisssions id to the next ID

--- src/main/resources/MySQL/201412091147_RSKSR-30.sql
+++ src/main/resources/MySQL/201412091147_RSKSR-30.sql
@@ -1,5 +1,5 @@
 INSERT INTO `permissions`(`id`,`name`,`description`,`controller`,`action`,`bizrule`) values 
-(33,'Add Multiple Components','Add Multiple Components','design','addMultipleComponents','');
+(34,'Add Multiple Components','Add Multiple Components','design','addMultipleComponents','');
 
 INSERT INTO `roles_has_permissions`(`permissions_id`,`roles_id`) values
-(33,1),(33,2),(33,3);
+(34,1),(34,2),(34,3);
