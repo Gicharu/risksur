eeaf12494c84f6e0d81a5d2c36388d5eb13e09ec
Thuku Wa Ndung'u <eric@tracetracker.com>
2015-01-20 10:25:27 +0300

    RSKSR-42: Added error checking. Added a header.

--- src/main/php/protected/views/options/add.php
+++ src/main/php/protected/views/options/add.php
@@ -1,5 +1,6 @@
 <?php
 $this->pageTitle = Yii::app()->name . ' - Add Option';
+echo "<h3>Add Option</h3>";
 echo $this->renderPartial('_form', array(
 		'model' => $model,
 		'surformdetailsArray' => $surformdetailsArray
