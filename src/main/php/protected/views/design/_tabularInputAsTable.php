a0114a3c726602374d1fa6d568b0a1cbc5aba849
James Njoroge <james@tracetracker.com>
2015-04-21 15:34:40 +0300

    RSKSR-38 Ensuring multiple componenet form validation shows errors

--- src/main/php/protected/views/design/_tabularInputAsTable.php
+++ src/main/php/protected/views/design/_tabularInputAsTable.php
@@ -1,20 +1,22 @@
 <?php
-	//$form->activeForm['id'] = "[$index]" . $form->activeForm['id'];
-	//print_r($form->activeForm['id']); die();
-	//echo $form->renderBegin();
-	//print_r($form); die();
-	foreach ($form->getElements() as $key => $element) {
-		// set the element name to add the row prefix
-		$element->name = "[$index]" . $key;
-		if($index == 0) {
-			//$element->class = " firstRow";
-		}
-		//print_r($element);
-		//die();
+//$form->activeForm['id'] = "[$index]" . $form->activeForm['id'];
+//print_r($form->activeForm['id']); die();
+//echo $form->renderBegin();
+//print_r($form); die();
+foreach ($form->getElements() as $key => $element) {
+    // set the element name to add the row prefix
+    $element->name = "[$index]" . $key;
+    if ($index == 0) {
+        //$element->class = " firstRow";
+    }
+//		print_r($element);
+//		die();
 
-		echo "<td>";
-		echo $element;
-		echo "</td>";
-	}
-	//echo $form->renderEnd();
+    echo "<td>";
+//		echo $element;
+    echo $element->render();
+    //echo CHtml::error($form->getModel(), $element->label);
+    echo "</td>";
+}
+//echo $form->renderEnd();
 ?>
