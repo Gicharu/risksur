7da3aba67fc9d3755940acdaf0b18987071786bc
James Njoroge <james@tracetracker.com>
2015-05-11 18:33:33 +0300

    Checkstyle fixes

--- src/main/php/protected/views/layouts/column2.php
+++ src/main/php/protected/views/layouts/column2.php
@@ -10,33 +10,33 @@
 				      'active' => strstr('create', $currentView)),
 				array('label'  => Yii::t("translation", "List Existing Systems"), 'url' => array('list'),
 				      'active' => strstr('list', $currentView)),
-				array('label' => Yii::t("translation", "List Components"), 'url' => array('design/listComponents'),
+				array('label'       => Yii::t("translation", "List Components"), 'url' => array('design/listComponents'),
 				      'itemOptions' => array(
-					'id' => 'showComponents'
-				)),
+					      'id' => 'showComponents'
+				      )),
 			);
 		}
 		// Setup Design controller side menus
 		if (Yii::app()->controller->id == 'design') {
 
 			$this->menu = array(
-				array('label'  => Yii::t("translation", "Add Component"), 'url' => array('addComponent'),
+				array('label'       => Yii::t("translation", "Add Component"), 'url' => array('addComponent'),
 				      'itemOptions' => array(
-					'id' => 'addComponent'
-				),
-				      'active' => strstr('addComponent', $currentView),
+					      'id' => 'addComponent'
+				      ),
+				      'active'      => strstr('addComponent', $currentView),
 				),
 				array('label'  => Yii::t("translation", "Add Multiple Components"),
-				      'url' => array('addMultipleComponents'), 'itemOptions' => array(
+				      'url'    => array('addMultipleComponents'), 'itemOptions' => array(
 					'id' => 'addMultipleComponents'
 				),
 				      'active' => strstr('addMultipleComponents', $currentView),
 				),
-				array('label'  => Yii::t("translation", "List Components"), 'url' => array('listComponents'),
+				array('label'       => Yii::t("translation", "List Components"), 'url' => array('listComponents'),
 				      'itemOptions' => array(
-					'id' => 'showComponents'
-				),
-				      'active' => strstr('listComponents', $currentView),
+					      'id' => 'showComponents'
+				      ),
+				      'active'      => strstr('listComponents', $currentView),
 				),
 				//array('label'=>'Manage SurFormDetails', 'url'=>array('admin')),
 			);
@@ -47,7 +47,7 @@
 			$currentController = Yii::app()->controller->id;
 			$this->menu = array(
 				array('label' => Yii::t("translation", "Introduction to Evaluation of Surveillance "),
-				      'url' => array('evaPage'),
+				      'url'   => array('evaPage'),
 				      'items' => array(
 					      array('label' => 'The EVA Tool', 'url' => array("$currentController/evaPage")),
 					      array('label' => 'Evaluation Concepts', 'url' => array("$currentController/evaConcept")),
@@ -63,15 +63,15 @@
 				array('label' => Yii::t("translation", "Evaluation attributes and economic criteria"),
 				      'url'   => array("$currentController/index"), 'items' => array(
 					array('label' => Yii::t("translation", "Economic evaluation"),
-					      'url' => array("$currentController/index")),
+					      'url'   => array("$currentController/index")),
 					array('label' => Yii::t("translation", "Epidemiological assessment"),
-					      'url' => array("$currentController/index")),
+					      'url'   => array("$currentController/index")),
 					array('label' => 'Final list of attributes', 'url' => array("$currentController/index"))
 				),
 				),
 
 				array('label' => Yii::t("translation", "Summary and Report"),
-				      'url' => array("$currentController/index")),
+				      'url'   => array("$currentController/index")),
 			);
 		}
 		//        $this->beginWidget('zii.widgets.CPortlet', array(
